276d5b50661be9559013a4eea4ddf191c3fae244
Alexey Rumyantsev <thelegendofflaity@gmail.com>
2024-03-10 11:32:42 +0300

    added task_2 calculations. improved task_2 static

--- fm_lab1/task_2/graph/complex_func.py
+++ fm_lab1/task_2/graph/complex_func.py
@@ -3,13 +3,19 @@ import sys
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-task_1_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
-sys.path.append(task_1_dir)
+task_2_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(task_2_dir)
 
-# from static import 
+from static import R, T, t, gap_1, gap_2, gap_3, gap_4, gap_len
 import calculations as calcs
 import sympy as sp
 import seaborn as sns
 
 sns.set_theme()
 sns.set_style("whitegrid", {'grid.linestyle': '--'})
+
+f_c_1 = R + (8 * R * t / T) * 1j
+f_c_2 = 2 * R - (8 * R * t / T) + R * 1j
+f_c_3 = -R + (4 * R - (8 * R * t / T)) * 1j
+f_c_4 = -6 * R + (8 * R * t / T) - R * 1j
+

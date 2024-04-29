3e66203c44169253e09e203f1e0bbc8b217af855
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2024-04-29 13:05:11 +0300

    renders

--- fm_lab3/nospec.py
+++ fm_lab3/nospec.py
@@ -9,37 +9,73 @@ time = st.time
 freq = st.freq
 g_fs = st.g_fs
 
-b = 3
-c = 1.5
+b = 1
+c = 2
 d = 1
-v_0 = 5
-v_0_2 = [[-0.74, -0.39], [0.39, 0.74]]
-
+v_0 = 0.39
+v_0_1 = 0.39
+v_0_2 = [[-0.77, -0.39], [0.39, 0.77]]
 u = hp.u_f(g_fs, time, b, c, d)
-flt_u, flt_U = ft.filter_high(freq, u, v_0)
-flt_u_0, flt_U_0 = ft.filter_special(freq, u, v_0_2)
-flt_u_2, flt_U_2 = ft.filter_special(freq, flt_u, v_0_2)
+
+doU = True
+doHigh = False
+doLow = False
+doSpecial = True
+
+flt_u, flt_U = None, None
+flt_u_0, flt_U_0 = None, None
+flt_u_1, flt_U_1 = None, None
+flt_u_2, flt_U_2 = None, None
 
 
-bd.build_u_or_U(time, u, xlab='Time', title='Noisy signal', legend=False, fz1=12, fz2=6)
-bd.build_u_to_U(freq, u, title='fft noisy signal', legend=False, fz1=12, fz2=6, xl1=-20, xl2=20, yl1=-400, yl2=500)
+if doU:
+    bd.build_u_or_U(time, u, xlab='Time', title=f'Noisy signal. b={b}, c={c}, d={d}', legend=False, fz1=12, fz2=6)
+    bd.build_u_to_U(freq, u, title=f'fft noisy signal. b={b}, c={c}, d={d}',
+                    legend=False, fz1=12, fz2=6,
+                    xl1=-10, xl2=10)
 
-bd.build_u__flt_u(time, u, flt_u,
-                  title=rf'High frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0}', fz1=12, fz2=6)
-bd.build_abs_u_to_U__flt_U(freq, u, flt_U,
-                           title=rf'Abs high frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0}', fz1=12, fz2=6,
-                           xl1=-30, xl2=30)
+if doHigh:
+    flt_u, flt_U = ft.filter_high(freq, u, v_0)
+    bd.build_u__flt_u(time, u, flt_u,
+                      title=rf'High frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0}', fz1=12, fz2=6)
+    bd.build_abs_u_to_U__flt_U(freq, u, flt_U,
+                               title=rf'Abs high frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0}', fz1=12, fz2=6,
+                               xl1=-25, xl2=25)
 
-bd.build_u__flt_u(time, u, flt_u_0,
-                  title=rf'Special frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0_2}', fz1=12, fz2=6)
-bd.build_abs_u_to_U__flt_U(freq, u, flt_U_0,
-                           title=rf'Abs special frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0_2}', fz1=12, fz2=6,
-                           xl1=-30, xl2=30)
 
-bd.build_u__flt_u(time, u, flt_u_2,
-                  title=rf'High and special frequency filter. b={b}, c={c}, d={d}, (1) $\nu_0$={v_0}, (2) $\nu_0$={v_0_2}',
-                  fz1=12, fz2=6)
-bd.build_abs_u_to_U__flt_U(freq, u, flt_U_2,
-                           title=rf'Abs high and special frequency filter. b={b}, c={c}, d={d}, (1) $\nu_0$={v_0}, (2) $\nu_0$={v_0_2}',
-                           fz1=12, fz2=6, xl1=-30,
-                           xl2=30)
+if doLow:
+    flt_u_1, flt_U_1 = ft.filter_low(freq, u, v_0_1)
+    bd.build_u__flt_u(time, u, flt_u_1,
+                      title=rf'Low frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0_1}', fz1=12, fz2=6)
+    bd.build_abs_u_to_U__flt_U(freq, u, flt_U_1,
+                               title=rf'Abs low frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0_1}', fz1=12, fz2=6,
+                               xl1=-25, xl2=25)
+
+if doSpecial:
+    flt_u_0, flt_U_0 = ft.filter_special(freq, u, v_0_2)
+    bd.build_u__flt_u(time, u, flt_u_0,
+                      title=rf'Special frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0_2}', fz1=12, fz2=6)
+    bd.build_abs_u_to_U__flt_U(freq, u, flt_U_0,
+                               title=rf'Abs special frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v_0_2}', fz1=12, fz2=6,
+                               xl1=-10, xl2=10)
+
+if doHigh and doSpecial:
+    flt_u_2, flt_U_2 = ft.filter_special(freq, flt_u, v_0_2)
+    bd.build_u__flt_u(time, u, flt_u_2,
+                      title=rf'High and special frequency filter. b={b}, c={c}, d={d}, (1) $\nu_0$={v_0}, (2) $\nu_0$={v_0_2}',
+                      fz1=12, fz2=6)
+    bd.build_abs_u_to_U__flt_U(freq, u, flt_U_2,
+                               title=rf'Abs high and special frequency filter. b={b}, c={c}, d={d}, (1) $\nu_0$={v_0}, (2) $\nu_0$={v_0_2}',
+                               fz1=12, fz2=6, xl1=-25,
+                               xl2=25)
+
+
+if doLow and doSpecial:
+    flt_u_2, flt_U_2 = ft.filter_special(freq, flt_u_1, v_0_2)
+    bd.build_u__flt_u(time, u, flt_u_2,
+                      title=rf'Low and special frequency filter. b={b}, c={c}, d={d}, (1) $\nu_0$={v_0_1}, (2) $\nu_0$={v_0_2}',
+                      fz1=12, fz2=6)
+    bd.build_abs_u_to_U__flt_U(freq, u, flt_U_2,
+                               title=rf'Abs low and special frequency filter. b={b}, c={c}, d={d}, (1) $\nu_0$={v_0_1}, (2) $\nu_0$={v_0_2}',
+                               fz1=12, fz2=6, xl1=-25,
+                               xl2=25)

06b27b9da3920f9b2f71e47989d78b13734c6acb
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2024-04-30 12:28:02 +0300

    latex

--- fm_lab3/audio.py
+++ fm_lab3/audio.py
@@ -7,8 +7,24 @@ import filters as ft
 import builder as bd
 
 
+filter_all = True
+build_U = False
+
+title = 'Low frequency filter audio'
+title2 = 'Abs low frequency filter audio'
+if filter_all:
+    title = 'Low and special frequency filter audio'
+    title2 = 'Abs low and special frequency filter audio'
+
 src = 'sound/MUHA.wav'
-audio, rate = librosa.load(src, sr=None)
+filename = 'sound/filtered_MUHA_2.wav'
+try:
+    audio, rate = librosa.load(src, sr=None)
+except:
+    lab = 'fm_lab3/'
+    src = lab + src
+    filename = lab + filename
+    audio, rate = librosa.load(src, sr=None)
 
 dt = 1 / rate
 T = len(audio) * dt
@@ -17,23 +33,25 @@ time = np.linspace(0, T, len(audio), endpoint=False)
 freq = np.linspace(-rate / 2, rate / 2, len(audio), endpoint=False)
 
 flt_u, flt_U = ft.filter_low(freq, audio, 300)
+if filter_all:
+    flt_u, flt_U = ft.filter_special(freq, flt_u, [[freq[0], -1000], [1000, freq[-1]]])
 flt_u_float = flt_u.real.astype(np.float32)
 
 
-bd.build_u_or_U(time, audio, xlab='Time',
-                title='Noisy audio signal', fz1=12, fz2=6,
-                legend=False)
-bd.build_u_to_U(freq, audio, title='fft noisy audio signal',
-                legend=False, fz1=12, fz2=6,
-                xl1=-875, xl2=875, yl1=-4000,
-                yl2=4000)
+if build_U:
+    bd.build_u_or_U(time, audio, xlab='Time',
+                    title='Noisy audio signal', fz1=12, fz2=6,
+                    legend=False)
+    bd.build_u_to_U(freq, audio, title='fft noisy audio signal',
+                    legend=False, fz1=12, fz2=6,
+                    xl1=-875, xl2=875, yl1=-4000,
+                    yl2=4000)
 
 bd.build_u__flt_u(time, audio, flt_u,
-                  title='Low frequency filter audio', fz1=12, fz2=6)
+                  title=title, fz1=12, fz2=6)
 bd.build_abs_u_to_U__flt_U(freq, audio, flt_U,
-                           title='Abs low frequency filter audio', xl1=-750, xl2=750,
+                           title=title2, xl1=-1500, xl2=1500,
                            yl1=0, yl2=2000, fz1=12, fz2=6)
 
-filename = 'sound/filtered_MUHA.wav'
 write(filename, rate, flt_u_float)
 playsound(filename)

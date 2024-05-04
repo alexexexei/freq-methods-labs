0f2953d78a66de2cc45d47ab30b4f37037fb84f3
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2024-05-04 22:51:35 +0300

    simplify, added LAFR

--- fm_lab4/task2.py
+++ fm_lab4/task2.py
@@ -31,60 +31,46 @@ def W_2f(w, T_1, T_2, T_3):
     return (T_1 * p + 1) ** 2 / ((T_2 * p + 1) * (T_3 * p + 1))
 
 
-def perform_1(t, v, w, u, a, b, c, d, T_0, g_f=None):
+def get_LAFR(W):
+    return 20 * np.log10(np.abs(W))
+
+
+def perform(t, v, w, 
+            u, a, b, 
+            c, d, T, 
+            W, LAFR, g_f=None, 
+            shift=True, name='Filter'):
     U = np.fft.fft(u)
-    W = W_1f(w, T_0)
-    flt_U = np.fft.fftshift(W) * U
+    flt_U = None
+    if shift:
+        flt_U = np.fft.fftshift(W) * U
+    else:
+        flt_U = W * U
     flt_u = np.fft.ifft(flt_U)
 
     sh_abs_U = abs(np.fft.fftshift(U))
     sh_abs_flt_U = abs(np.fft.fftshift(flt_U))
     flist1 = [u, flt_u.real]
-    if g_f is not None:
-        flist1.append(g_f)
     flist2 = [sh_abs_U, sh_abs_flt_U]
     llist1 = ['Original signal', 'Filtered signal']
-    if g_f is not None:
-        llist1.append(r'Original function $g(t)$')
     llist2 = ['Abs original signal fft', 'Abs filtered signal fft']
+    lcolors = [None, None]
     abs_W = abs(W)
-
-    bf.build_fs(t, y=flist1, labels=llist1,
-                ttl=rf'First order filter. a={a}, b={b}, c={c}, d={d}, $T_0={T_0}$', 
-                xlab='Time', ylab='Amplitude', legend=True)
-    bf.build_fs(v, y=flist2, labels=llist2, 
-                ttl=rf'First order filter abs fft. a={a}, b={b}, c={c}, d={d}, $T_0={T_0}$', 
-                xlab='Frequency', ylab='Amplitude', legend=True)
-    bf.build_f(w, abs_W, ttl=rf'First order filter amplitude-frequency response. a={a}, b={b}, c={c}, d={d}, $T_0={T_0}$',
-               xlab='Angular frequency', ylab='Amplitude-frequency response')
-
-
-def perform_2(t, v, w, u, a, b, c, d, T_1, T_2, T_3, g_f=None):
-    U = np.fft.fft(u)
-    W = W_2f(w, T_1, T_2, T_3)
-    flt_U = W * U
-    flt_u = np.fft.ifft(flt_U)
-
-    sh_abs_U = abs(np.fft.fftshift(U))
-    sh_abs_flt_U = abs(np.fft.fftshift(flt_U))
-    flist1 = [u, flt_u.real]
     if g_f is not None:
         flist1.append(g_f)
-    flist2 = [sh_abs_U, sh_abs_flt_U]
-    llist1 = ['Original signal', 'Filtered signal']
-    if g_f is not None:
         llist1.append(r'Original function $g(t)$')
-    llist2 = ['Abs original signal fft', 'Abs filtered signal fft']
-    abs_W = abs(W)
+        lcolors.append('r')
 
-    bf.build_fs(t, y=flist1, labels=llist1,
-                ttl=rf'Special filter. a={a}, b={b}, c={c}, d={d}, $T_1={T_1},T_2={T_2},T_3={T_3}$', 
+    bf.build_fs(t, y=flist1, labels=llist1, colors=lcolors,
+                ttl=f'{name}. a={a}, b={b}, c={c}, d={d}, T={T}', 
                 xlab='Time', ylab='Amplitude', legend=True)
     bf.build_fs(v, y=flist2, labels=llist2, 
-                ttl=rf'Special filter abs fft. a={a}, b={b}, c={c}, d={d}, $T_1={T_1},T_2={T_2},T_3={T_3}$', 
+                ttl=f'{name} abs fft. a={a}, b={b}, c={c}, d={d}, T={T}', 
                 xlab='Frequency', ylab='Amplitude', legend=True)
-    bf.build_f(w, abs_W, ttl=rf'Special filter amplitude-frequency response. a={a}, b={b}, c={c}, d={d}, $T_1={T_1},T_2={T_2},T_3={T_3}$', 
+    bf.build_f(w, y=abs_W, ttl=f'{name} amplitude-frequency response. a={a}, b={b}, c={c}, d={d}, T={T}',
                xlab='Angular frequency', ylab='Amplitude-frequency response')
+    bf.build_f(w, y=LAFR, ttl=f'{name} logarithmic amplitude frequency response. a={a}, b={b}, c={c}, d={d}, T={T}',
+               xlab='Angular frequency', ylab='Amplitude')
 
 
 T = 10
@@ -110,11 +96,17 @@ g_fun = get_g_f(t, t_1, t_2, a)
 u = get_u(g_fun, t, b, c, d)
 
 T_0 = 1
+W_1 = W_1f(w, T_0)
+LAFR = get_LAFR(W_1)
 
-perform_1(t, v, w, u, a, b, c, d, T_0, g_fun)
+perform(t, v, w, 
+        u, a, b, 
+        c, d, T_0, 
+        W_1, LAFR, g_f=g_fun,
+        name='First order filter')
 
 
-a = 10
+a = 2
 b = 0
 c = 10
 d = 15
@@ -122,8 +114,14 @@ d = 15
 g_fun = get_g_f(t, t_1, t_2, a)
 u = get_u(g_fun, t, b, c, d)
 
-T_1 = 0.4
-T_2 = 0.8
-T_3 = 1
+T_1 = 0.2
+T_2 = 0.5
+T_3 = 0.1
+W_2 = W_2f(w, T_1, T_2, T_3)
+LAFR = get_LAFR(W_2)
 
-perform_2(t, v, w, u, a, b, c, d, T_1, T_2, T_3, g_fun)
+perform(t, v, w, 
+        u, a, b, 
+        c, d, [T_1, T_2, T_3], 
+        W_2, LAFR, g_f=g_fun, 
+        shift=False, name='Special filter')

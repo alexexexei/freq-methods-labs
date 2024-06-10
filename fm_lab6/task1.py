0e9112ee16f8520a03e7af243ce802624489a229
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2024-06-10 07:10:13 +0300

    com

--- fm_lab6/task1.py
+++ fm_lab6/task1.py
@@ -2,13 +2,14 @@ import img_utils as iu
 
 
 src = 'fm_lab6/src'
-img_path = f'{src}/4.png'
+curr_img = '2'
+img_path = f'{src}/{curr_img}.png'
 
 render_to = 'fm_lab6/renders/task1'
-rimg_path = f'{render_to}/fft2.png'
-reimg_path = f'{render_to}/new4.png'
+rimg_path = f'{render_to}/fft2_{curr_img}.png'
+reimg_path = f'{render_to}/new4_{curr_img}.png'
 
-corr_im_path = f'{src}/corr_fft2.png'
+corr_im_path = f'{src}/corr_fft2_{curr_img}.png'
 corrected = True
 
 img = iu.read_img(img_path)

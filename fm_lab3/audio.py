4107ebb02dea7a1ac39158fcf88be96abdcf6e3b
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2024-06-23 21:23:59 +0300

    fix lab 3 t2. Everything is fixed!

--- fm_lab3/audio.py
+++ fm_lab3/audio.py
@@ -21,6 +21,7 @@ time = np.linspace(0, T, len(audio), endpoint=False)
 freq = np.linspace(-rate / 2, rate / 2, len(audio), endpoint=False)
 
 flt_u, flt_U = ft.filter_high(freq, audio, 300)
+flt_u, flt_U = ft.filter_special_out(freq, flt_u, [[freq[0], -4500], [4500, freq[-1]]])
 flt_u_float = flt_u.real.astype(np.float32)
 
 bd.build_u_or_U(time,
@@ -38,8 +39,17 @@ bd.build_u_to_U(freq,
                 fz2=6,
                 xl1=0,
                 xl2=500)
+bd.build_u_to_U(freq,
+                audio,
+                title='fft noisy audio signal',
+                legend=False,
+                fz1=12,
+                fz2=6,
+                xl1=3000,
+                yl1=0.25,
+                yl2=5)
 
-bd.build_u__flt_u(time, audio, flt_u, title=title, fz1=12, fz2=6)
+bd.build_u__flt_u(time, audio.real, flt_u, title=title, fz1=12, fz2=6)
 bd.build_abs_u_to_U__flt_U(freq,
                            audio,
                            flt_U,
@@ -49,6 +59,15 @@ bd.build_abs_u_to_U__flt_U(freq,
                            xl1=0,
                            xl2=500,
                            yl2=2000)
+bd.build_abs_u_to_U__flt_U(freq,
+                           audio,
+                           flt_U,
+                           title=title2,
+                           fz1=12,
+                           fz2=6,
+                           xl1=3000,
+                           yl1=-0.25,
+                           yl2=5)
 
 write(filename, rate, flt_u_float)
 playsound(filename)

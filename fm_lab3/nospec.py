f00b94894197195e3efa9ce23382eb5bca669afd
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2024-06-23 20:35:11 +0300

    fix lab 3 t1.2

--- fm_lab3/nospec.py
+++ fm_lab3/nospec.py
@@ -9,9 +9,11 @@ time = st.time
 freq = st.freq
 g_fs = st.g_fs
 
-b, c, d = 1.5, -2, -3
-v0, v01 = [[-0.52, -0.3], [0.3, 0.52]], 10
+b, c, d = 0.5, 10, 20
 u = hp.u_f(g_fs, time, b, c, d)
+
+v = [2.594, 3.858]
+v0, v01 = [[-v[1], -v[0]], v], 3.8
 
 flt_u_so, flt_U_so = ft.filter_special_out(freq, u, v0)
 bd.build_u__flt_u(
@@ -28,7 +30,7 @@ bd.build_abs_u_to_U__flt_U(
     title=rf'Abs special frequency filter. b={b}, c={c}, d={d}, $\nu_0$={v0}',
     fz1=12,
     fz2=6,
-    xl1=-10,
+    xl1=0,
     xl2=10)
 
 flt_u_lso, flt_U_lso = ft.filter_low(freq, flt_u_so, v01)
@@ -48,5 +50,5 @@ bd.build_abs_u_to_U__flt_U(
     rf'Abs low and special frequency filter. b={b}, c={c}, d={d}, (1) $\nu_0$={v01}, (2) $\nu_0$={v0}',
     fz1=12,
     fz2=6,
-    xl1=-10 - v01,
-    xl2=10 + v01)
+    xl1=0,
+    xl2=10)
